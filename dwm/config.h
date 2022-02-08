9f5473c9783dadfb04569f63b3ad3a3902de7ec4
Diabltica <Diabltica@gmail.com>
2022-02-08 20:48:47 +0100

    Change terminal from st to kitty

--- dwm/config.h
+++ dwm/config.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_salmon, "-sf", col_lightyellow, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
