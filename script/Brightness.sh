3bf53f380a62d12bf91fea4417ab9fc400122cfd
Diabltica <Diabltica@gmail.com>
2022-02-25 18:55:40 +0100

    Change variable name

--- script/Brightness.sh
+++ script/Brightness.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #echo current Brightness
-current=$(xrandr --verbose --current | grep -A10 " connected" | grep "Brightness" | cut -d ':' -f 2)
-
+current_brightness=$(xrandr --verbose --current | grep -A10 " connected" | grep "Brightness" | cut -d ':' -f 2)
+screen_connected=$(xrandr --verbose --current | grep " connected" | cut -d " " -f 1)
 
 if [[ -z "$2" ]]; then
   value="0.1"
@@ -10,19 +10,19 @@ else
   value=$2
 fi
 
-brightness_value=$current
+brightness_value=$current_brightness
 
 case $1 in 
   -i|--increment)
-    if [[ $current < 1 ]]; then
-      brightness_value=$(echo "$current+$value"|bc)
+    if [[ $current_brightness < 1 ]]; then
+      brightness_value=$(echo "$current_brightness+$value"|bc)
     fi
   ;;
   -d|-decrement)
-    if [[ $current > 0.3 ]]; then
-      brightness_value=$(echo "$current-$value"|bc)
+    if [[ $current_brightness > 0.3 ]]; then
+      brightness_value=$(echo "$current_brightness-$value"|bc)
     fi
   ;;
 esac
-
-xrandr --output eDP-1 --brightness $brightness_value
+echo $screen_connected
+xrandr --output $screen_connected --brightness $brightness_value
